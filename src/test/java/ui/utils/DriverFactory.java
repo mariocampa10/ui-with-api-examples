722a783856a4adeeec9f2c2df69f8bf87ecd9392
mariocampa10 <mariocampa10@gmail.com>
2024-01-29 17:24:49 +0100

    Remove WebdriverManager
    WebdriverManager is no longer needed for latest versions of Webdriver

--- src/test/java/ui/utils/DriverFactory.java
+++ src/test/java/ui/utils/DriverFactory.java
@@ -1,25 +1,19 @@
 package ui.utils;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
-
-import static ui.utils.BrowserType.*;
 
 public class DriverFactory {
 
     public static WebDriver getDriver(BrowserType browserType) {
         switch (browserType) {
             case FIREFOX:
-                WebDriverManager.firefoxdriver().setup();
                 return new FirefoxDriver();
             case SAFARI:
-                WebDriverManager.safaridriver().setup();
                 return new SafariDriver();
             default:
-                WebDriverManager.chromedriver().setup();
                 return new ChromeDriver();
         }
     }
